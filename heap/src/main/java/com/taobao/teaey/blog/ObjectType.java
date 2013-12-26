af84b0d2c2ca239ee116fd2a471e95747ef0e776
空正 <xiaofei.wxf@alibaba-inc.com>
2013-12-26 15:52:12 +0800

    init

---
+++ heap/src/main/java/com/taobao/teaey/blog/ObjectType.java
@@ -0,0 +1,12 @@
+package com.taobao.teaey.blog;
+
+public interface ObjectType {
+  public void navigate(int index);
+	public void setInt(int value);
+	public void setLong(long value);
+	public void setByte(byte value);
+	
+	public int getInt();
+	public long getLong();
+	public byte getByte();
+}
---
+++ heap/src/main/java/com/taobao/teaey/blog/TestMemoryAllocator.java
@@ -0,0 +1,77 @@
+package com.taobao.teaey.blog;/*
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import java.util.concurrent.TimeUnit;
+
+public class TestMemoryAllocator {
+
+  public static void main(String...args)
+	{
+		int element = Integer.valueOf(args[1]);
+		ObjectType[] types = new ObjectType[]{ Allocator.allocate(Allocator.Type.valueOf(args[0]), element)};
+		int ONE_MILLION = 1000000;
+		
+		for(int x = 0;x<50;x++)
+		{
+			for(ObjectType t : types)
+			{
+				long writeStart = System.nanoTime();
+				write(t,element);
+				long totalWrite = System.nanoTime() - writeStart;
+				
+				
+				long readStart = System.nanoTime();
+				read(t,element);
+				long totalRead = System.nanoTime() - readStart;
+				
+				double writeMs = totalWrite/1000000d;
+				double readMs = totalRead/1000000d;
+				
+				System.out.println(String.format("[%s] %s - [Write %s ms , Read %s ms ], Op/Sec(Millions)[ Write %s , Read %s ]",
+						x,t.getClass().getName(), writeMs,readMs,
+						(TimeUnit.SECONDS.toNanos(element)/totalWrite)/ONE_MILLION,
+						(TimeUnit.SECONDS.toNanos(element)/totalRead)/ONE_MILLION));
+			}		
+		}
+	}
+	
+	
+	public static void write(ObjectType t,int items)
+	{
+		for(int index=0;index<items;index++)
+		{
+			t.navigate(index);
+			t.setByte((byte)index);
+			t.setInt(index);
+			t.setLong(index * index);
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	public static void read(ObjectType t,int items)
+	{
+		int intSum=0;
+		int longSum=0;
+		for(int index=0;index<items;index++)
+		{
+			t.navigate(index);
+			t.getByte();
+			intSum+=t.getInt();
+			longSum+=t.getLong();
+		}
+		
+	}
+}
