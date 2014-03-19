4e86b3f440f74a07079a0344461495513e07bb44
空正 <xiaofei.wxf@alibaba-inc.com>
2014-03-19 18:04:14 +0800

    add serviceloader

---
+++ java/src/main/java/classloader/ContextClassLoader.java
@@ -0,0 +1,15 @@
+package classloader;
+
+import javax.naming.Context;
+
+/**
+ * Created by xiaofei.wxf on 2014/3/19.
+ */
+public class ContextClassLoader {
+    public static void main(String[] args) {
+        ClassLoader cl1 = Thread.currentThread().getContextClassLoader();
+        ClassLoader cl2 = ContextClassLoader.class.getClassLoader();
+        ClassLoader cl3 = ClassLoader.getSystemClassLoader().getParent();
+        System.out.println(cl1 == cl2);
+    }
+}
