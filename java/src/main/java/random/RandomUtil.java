60799a78954b0eb2204fe19219731dba268f433a
xiaofei.wxf <xiaofei.wxf@alibaba-inc.com>
2014-05-05 16:35:43 +0800

    add random util

--- java/src/main/java/random/RandomUtil.java
+++ java/src/main/java/random/RandomUtil.java
@@ -1,10 +1,26 @@
 package random;
 
-import java.util.Arrays;
 import java.util.Map;
 
 /**
- * Created by wxf on 14-5-5.
+ * 随机工具类
+ *
+ * 使用权重的集合Map构建随机元数据对象
+ *
+ * 比如：
+ * 我们有3个url地址，他们的权重分别为1,2,3现在我们利用RandomUtil来根据权重随机获取url：
+ *
+ * <p><blockquote><pre>
+ *
+ * map.put(url1, 1);
+ * map.put(url2, 2);
+ * map.put(url3, 3);
+ * RandomMeta<String, Integer> md = RandomUtil.buildWeightMeta(map);
+ * String weightRandomUrl = md.random();
+ *
+ * </pre></blockquote><p>
+ *
+ * @author wxf on 14-5-5.
  */
 public class RandomUtil {
     public static <T> WeightMeta<T> buildWeightMeta(final Map<T, Integer> weightMap) {
